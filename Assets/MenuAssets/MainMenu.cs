1aa740f4f7d9441d3a514519cc900d537ea83d92
ZekLun <zeke.lundgren@arlandagymnasiet.se>
2025-05-05 12:35:25 +0200

    Added Difficulties
    - Made another copy of gamescene
     - Now exists EasyGame scene and HardGame scene.
    - Made some differences between enemies in difficulties

--- Assets/MenuAssets/MainMenu.cs
+++ Assets/MenuAssets/MainMenu.cs
@@ -6,8 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame()
+    public void PlayEasyGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayHardGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
