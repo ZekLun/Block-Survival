99c324854854efc1f454f15dd5ba04f2fe50e30a
Zenktify <zeke.lundgren@arlandagymnasiet.se>
2025-06-12 16:41:05 +0200

    Pause menu

--- Assets/GameAssets/UIScript.cs
+++ Assets/GameAssets/UIScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection.Emit;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIScript : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class UIScript : MonoBehaviour
     static public int HighScore = 0;
     public TextMeshProUGUI CoinText;
     public TextMeshProUGUI HighScoreText;
+    public GameObject PauseScreen;
+    private bool menuActive = false;
 
 
     private void Start()
@@ -26,6 +29,25 @@ public class UIScript : MonoBehaviour
             HighScore = Coins;
             PlayerPrefs.SetInt("HighScore", Coins);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuActive = !menuActive;
+            PauseScreen.SetActive(menuActive);
+            Time.timeScale = menuActive ? 0 : 1;
+        }
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Continue()
+    {
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1;
+        menuActive = false;
     }
 
 }
